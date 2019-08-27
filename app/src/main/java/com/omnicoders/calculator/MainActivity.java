712906fe8185b6122aba88bb110430191fd68b9d
dejegithub <dtechane6@gmail.com>
2019-08-27 22:13:16 +0300

    Update the MainActivity class

--- app/src/main/java/com/omnicoders/calculator/MainActivity.java
+++ app/src/main/java/com/omnicoders/calculator/MainActivity.java
@@ -1,13 +1,196 @@
 package com.omnicoders.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //Decalare all widgets here
+    TextView showResult;
+    Button btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    Button btnDiv, btnPlus, btnMult, btnMinus, btnDot, btnClear, btnMod, btnEqual;
 
+    String str = "";
+    Character op = 'q';
+    int num, num1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //get button widgets by ids
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btnDot = findViewById(R.id.btnDot);
+        btnDiv = findViewById(R.id.btnDiv);
+        btnMult = findViewById(R.id.btnMult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMod = findViewById(R.id.btnMod);
+        btnClear = findViewById(R.id.btnClear);
+        btnEqual = findViewById(R.id.btnequal);
+        showResult = findViewById(R.id.txtResult);
+
+        //calling the method PerformCalculator()
+        performCalculator();
+    }
+
+    //Method to perform ActionListener
+    private void performCalculator() {
+        //Apply action listener on buttons
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(0);
+            }
+        });
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(1);
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(2);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(3);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(4);
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(5);
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(6);
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(7);
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(8);
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                insert(9);
+            }
+        });
+
+        btnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '+';
+            }
+        });
+        btnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '-';
+            }
+        });
+        btnMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '*';
+            }
+        });
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '/';
+            }
+        });
+        btnMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                perform();
+                op = '%';
+            }
+        });
+        btnEqual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calculate();
+            }
+        });
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                reset();
+            }
+        });
+    }
+
+    private void reset() {
+        str = "";
+        op = 'q';
+        num = 0;
+        num1 = 0;
+        showResult.setText("");
+    }
+
+    private void calculate() {
+        if (op == '+')
+            num = num1 + num;
+        else if (op == '-')
+            num = num1 - num;
+        else if (op == '*')
+            num = num1 * num;
+        else if (op == '/')
+            num = num1 / num;
+        showResult.setText("" + num);
+
+    }
+
+    private void perform() {
+        str = "";
+        num1 = num;
+
+    }
+
+    private void insert(int j) {
+        str = str + j;
+        num = Integer.valueOf(str).intValue();
+        showResult.setText(str);
+
     }
 }
